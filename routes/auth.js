cb23b615be853e967da75b47a8a4106e5a8e6c97
Rajesh kumar <prince.rk.cool@gmail.com>
2021-04-04 14:55:06 +0530

    initial commit

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const AuthController = require("../controllers/AuthController");
-
-router.post("/login", AuthController.login);
-router.get("/user", AuthController.user);
-
-module.exports = router;
